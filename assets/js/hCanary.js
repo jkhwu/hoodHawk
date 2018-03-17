73f6ef91cf9f9a6dd6508741b05f77b7fb829c0a
andrew buckley <andrewbuckley@abuck-macbook.local>
2018-03-17 12:51:52 -0700

    initialized firebase, added external js links. minor rough sketch of how to layout housecanary js file

---
+++ assets/js/hCanary.js
@@ -0,0 +1,14 @@
+hCanaryObj = {
+
+    auth : {
+        hc_api_key: "3LSLSSTG46BO8P7H96I4" ,
+        hc_api_secret: "XnqjMq7SujGcGlS05WCa4THVGNhpAU4a"
+    } ,
+
+    geocodeURL : "https://api.housecanary.com/v2/property/geocode?address=10216+N+Willow+Ave&zipcode=64157" ,
+    valueURL : "https://api.housecanary.com/v2/property/value?address=10216+N+Willow+Ave&zipcode=64157" ,
+
+    getGeocode : function() {
+
+    }
+}
