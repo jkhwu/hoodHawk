c3ba953e7d4f840be32bb0b67c91d1e926992e89
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-03-18 12:57:11 -0700

    a

--- assets/js/hCanary.js
+++ assets/js/hCanary.js
@@ -1,30 +1,3 @@
-/*hCanaryObj = {
-
-    key : '3LSLSSTG46BO8P7H96I4' ,
-    secret : 'XnqjMq7SujGcGlS05WCa4THVGNhpAU4a' ,
-    url : "https://cors-anywhere.herokuapp.com/https://api.housecanary.com/v2/property/value?address=10216+N+Willow+Ave&zipcode=64157" ,
-    
-   
-   details : function() {
-    $.ajax({
-        url: this.url ,
-        type: 'GET',
-        beforeSend: function (xhr) {
-            console.log(this)
-            console.log(hCanaryObj)
-            xhr.setRequestHeader(
-                'Authorization',
-                'Basic ' + btoa(hCanaryObj.key + ":" + hCanaryObj.secret)
-            )
-        },
-        data: {}
-    }).then(function(data) { 
-        console.log(data)
-    });
-    }
-}
-
-hCanaryObj.details()*/
 var block = ''
 var blockGroup = ''
 var zip = ''
