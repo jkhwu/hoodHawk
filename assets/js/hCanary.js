bba01a98617a06df6071683ece96f5286541b894
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-03-17 19:06:28 -0700

    keys for group, block, and zip

--- assets/js/hCanary.js
+++ assets/js/hCanary.js
@@ -1,4 +1,4 @@
-hCanaryObj = {
+/*hCanaryObj = {
 
     key : '3LSLSSTG46BO8P7H96I4' ,
     secret : 'XnqjMq7SujGcGlS05WCa4THVGNhpAU4a' ,
@@ -24,24 +24,95 @@ hCanaryObj = {
     }
 }
 
-hCanaryObj.details()
+hCanaryObj.details()*/
+var block = ''
+var blockGroup = ''
+var zip = ''
+initial()
 
+function initial() {
+    var key = '3LSLSSTG46BO8P7H96I4'
+    var secret = 'XnqjMq7SujGcGlS05WCa4THVGNhpAU4a';
+    var url = "https://cors-anywhere.herokuapp.com/https://api.housecanary.com/v2/property/details?address=1130huntingdondr&zipcode=95129"
 
-// var key = '3LSLSSTG46BO8P7H96I4'
-// var secret = 'XnqjMq7SujGcGlS05WCa4THVGNhpAU4a';
+    $.ajax({
+        url: url,
+        type: 'GET',
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader(
+                'Authorization',
+                'Basic ' + btoa(key + ":" + secret)
+            );
+        },
+        data: {}
+    }).then(function(data) {
+        console.log(data)
+        block = data[0].address_info.block_id
+        blockGroup = data[0].address_info.blockgroup_id
+        zip = data[0].address_info.zipcode
 
-// var url = "https://cors-anywhere.herokuapp.com/https://api.housecanary.com/v2/property/value?address=10216+N+Willow+Ave&zipcode=64157";
+        crime()
+        blockgroup()
+        zipdetail()
+    });
+}
 
-// $.ajax({
-//     url: url,
-//     type: 'GET',
-//     beforeSend: function (xhr) {
-//         xhr.setRequestHeader(
-//             'Authorization',
-//             'Basic ' + btoa(key + ":" + secret)
-//         );
-//     },
-//     data: {}
-// }).then(function(data) { 
-//     console.log(data);
-// });
+function crime() {
+    console.log(block)
+    console.log(blockGroup)
+    console.log(zip)
+    var key = '3LSLSSTG46BO8P7H96I4'
+    var secret = 'XnqjMq7SujGcGlS05WCa4THVGNhpAU4a';
+    var url = `https://cors-anywhere.herokuapp.com/https://api.housecanary.com/v2/block/crime?block_id=${block}`
+    $.ajax({
+        url: url,
+        type: 'GET',
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader(
+                'Authorization',
+                'Basic ' + btoa(key + ":" + secret)
+            );
+        },
+        data: {}
+    }).then(function(data) {
+        console.log(data)
+    });
+}
+
+function blockgroup() {
+    var key = '3LSLSSTG46BO8P7H96I4'
+    var secret = 'XnqjMq7SujGcGlS05WCa4THVGNhpAU4a';
+    var url = `https://cors-anywhere.herokuapp.com/https://api.housecanary.com/v2/blockgroup/hcri?blockgroup_id=${blockGroup}&property_type=SFD`
+    $.ajax({
+        url: url,
+        type: 'GET',
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader(
+                'Authorization',
+                'Basic ' + btoa(key + ":" + secret)
+            );
+        },
+        data: {}
+    }).then(function(data) {
+        console.log(data)
+    });
+}
+
+function zipdetail() {
+    var key = '3LSLSSTG46BO8P7H96I4'
+    var secret = 'XnqjMq7SujGcGlS05WCa4THVGNhpAU4a';
+    var url = `https://cors-anywhere.herokuapp.com/https://api.housecanary.com/v2/zip/details?zipcode=${zip}`
+    $.ajax({
+        url: url,
+        type: 'GET',
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader(
+                'Authorization',
+                'Basic ' + btoa(key + ":" + secret)
+            );
+        },
+        data: {}
+    }).then(function(data) {
+        console.log(data)
+    });
+}
