83890f2185dd9a0819eb0c7c400bddff2b8652f6
Janet Hwu <Janet@MacBook-Pro-2.local>
2018-03-21 16:58:52 -0700

    updating branch

--- assets/js/gMaps.js
+++ assets/js/gMaps.js
@@ -1,3 +1,6 @@
+
+// Create map on page load \\
+
 document.addEventListener('DOMContentLoaded', function() {
     if (document.querySelectorAll('#map').length > 0) {
         if (document.querySelector('html').lang)
@@ -7,41 +10,58 @@ document.addEventListener('DOMContentLoaded', function() {
 
         var js_file = document.createElement('script');
         js_file.type = 'text/javascript';
-        js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCaSVlHx6Mx2guFHDJUQTWgI-ZUf7dyUjw&callback=initMap&language=' + lang;
+        js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCaSVlHx6Mx2guFHDJUQTWgI-ZUf7dyUjw&callback=launchMap&language=' + lang;
         document.getElementsByTagName('head')[0].appendChild(js_file);
     }
 });
 
-var lat = 32.715736
-var lng = -117.161087
 
-function initMap() {
-    var myLocation = {
-        lat: +lat,
-        lng: +long
-    };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: +lat,
-            lng: +long
-        },
-        zoom: 14
-    });
-    var panorama = new google.maps.StreetViewPanorama(
-        document.getElementById('pano'), {
-            position: myLocation,
-            pov: {
-                heading: 34,
-                pitch: 10
-            }
-        });
-    //   var myMarker = new google.maps.Marker({
-    //       position: {
-    //         lat: 32.851721,
-    //         lng: -117.182813},
-    //       map: map,
-    //       icon: 'https://chart.googleapis.com/chart?chst=d_bubble_icon_text_small&chld=ski|bb|HERE|FFFFFF|000000',
-    //       title: 'myMarker'
-    //     });
-    map.setStreetView(panorama);
-}
+// Set map to initial point \\
+
+        function launchMap() {
+            var myLocation = {
+                lat: 37.421999900,
+                lng: -122.084057500
+            };
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                    lat: 37.421999900,
+                    lng: -122.084057500
+                },
+                zoom: 14
+            });
+            var panorama = new google.maps.StreetViewPanorama(
+                document.getElementById('pano'), {
+                    position: myLocation,
+                    pov: {
+                        heading: 34,
+                        pitch: 10
+                    }
+                });
+            map.setStreetView(panorama);
+        }
+
+// Set map to target address lat + long \\
+
+        function initMap() {
+            var myLocation = {
+                lat: +lat,
+                lng: +long
+            };
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                    lat: +lat,
+                    lng: +long
+                },
+                zoom: 14
+            });
+            var panorama = new google.maps.StreetViewPanorama(
+                document.getElementById('pano'), {
+                    position: myLocation,
+                    pov: {
+                        heading: 34,
+                        pitch: 10
+                    }
+                });
+            map.setStreetView(panorama);
+        }
